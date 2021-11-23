f67619c91e782ec64fda6da7f5fcf5d45f22188e
KoKoKotlin <labruzzler@gmail.com>
2021-11-23 15:53:01 +0100

    2nd ex

--- 03/main.py
+++ 03/main.py
@@ -1,7 +1,67 @@
-import tensorflow
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+import tensorflow.keras as keras
+import scipy.io
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+def ex41(xs, ys):
+
+    model = keras.Sequential(name="geilo")
+    
+    node_count = 1
+    input_shape = (1, 1)
+    
+    model.add(keras.layers.Dense(node_count, input_shape=input_shape))
+
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.1), loss="mean_absolute_error")
+
+    model.fit(xs, ys, epochs=5, batch_size=10, validation_split=0.2)
+    pxs = np.array([[0.0], [3.5]])
+    pys = model.predict(pxs)
+    
+    pys_ = []
+    for y in pys:
+        pys_.append(y[0][0])
+    
+    plt.scatter(xs, ys)
+    plt.plot([0.0, 3.5], pys_, color="r")
+    plt.show()
+
 
 def main():
-    pass
+    mat = scipy.io.loadmat("data_ex7.mat")
+    xs = mat["x"]
+    ys = mat["y"]
+    
+    ex41(xs, ys)
+    ex42(xs, ys)
+
+def ex42(xs, ys):
+    model = keras.Sequential(name="geilo2")
+    
+    node_count = 1
+    input_shape = (1, 1)
+    
+    model.add(keras.layers.Dense(node_count, input_shape=input_shape))
+    model.add(keras.layers.Dense(64))
+    model.add(keras.layers.Dense(1))
+
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.1), loss="mean_absolute_error")
+
+    model.fit(xs, ys, epochs=5, batch_size=10, validation_split=0.2)
+    pxs = np.array([[0.0], [3.5]])
+    pys = model.predict(pxs)
+    
+    pys_ = []
+    for y in pys:
+        pys_.append(y[0][0])
+    
+    plt.scatter(xs, ys)
+    plt.plot([0.0, 3.5], pys_, color="r")
+    plt.show()
 
 if __name__ == "__main__":
     main()
